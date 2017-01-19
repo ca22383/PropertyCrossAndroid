d3744d54b53dd613310a88d46a6a92ada12b7d66
Jose Luis Royo <jlroyo@iMac-de-Jose.local>
2017-01-19 17:25:27 +0100

    Primeros pasos

---
+++ app/src/main/java/com/example/ejemplo1jlroyo/propertycross/FilterSearchButtonActivity.java
@@ -0,0 +1,31 @@
+package com.example.ejemplo1jlroyo.propertycross;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FilterSearchButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_search_button);
+
+        Button ButtonSearch = (Button) findViewById(R.id.ButtonSearch);
+
+        ButtonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        ResultSearchActivity.class);
+                //intent.putExtra("numencerts", Resultat);
+                //intent.putExtra("numpreguntes", numPregunta +1);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
